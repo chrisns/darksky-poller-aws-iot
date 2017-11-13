c3dde30da985f0672d3cd385726212fd9b9eb5a2
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-11-13 00:43:52 +0000

    initial working build

---
+++ README.md
@@ -0,0 +1,3 @@
+#darksky iot lambda poller
+Needs the env vars of `LAT, LON, DARKSKY_API_KEY` made and iot things of `weather_currently`, `weather_minutely`, `weather_hourly`, `weather_daily`.
+It will periodically run and update a thing shadow with the current weather.
---
+++ main.js
@@ -0,0 +1,35 @@
+const AWS = require('aws-sdk')
+const request = require('request-promise')
+const {LAT, LON, DARKSKY_API_KEY} = process.env
+
+const handler = (event, context, callback) =>
+  get_weather()
+    .then(response =>
+      Promise.all(['currently', 'minutely', 'hourly', 'daily'].map(k =>
+        new Promise((resolve, reject) => {
+          new AWS.IotData({endpoint: process.env.AWS_IOT_ENDPOINT}).updateThingShadow({
+            thingName: `weather_${k}`,
+            payload: JSON.stringify({
+              state: {
+                desired: response[k]
+              }
+            })
+          }, (err, result) => {
+            if (err)
+              return reject(err)
+            resolve(result)
+          })
+        })
+      ))
+    )
+    .catch(err => callback(err))
+    .then(responses => callback(null, responses))
+
+const get_weather = () => request(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${LAT},${LON}?exclude=flags&units=uk2`)
+  .then(JSON.parse)
+  .then(response => {
+    response.hourly.data = response.hourly.data.slice(0, 24)
+    return response
+  })
+
+exports.handler = handler
