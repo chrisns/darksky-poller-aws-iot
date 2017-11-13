ae023365e9a164b2efad0e4e2a51952e7fa5a01f
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-11-13 07:27:14 +0000

    tidy up

--- main.js
+++ main.js
@@ -5,25 +5,25 @@ const {LAT, LON, DARKSKY_API_KEY} = process.env
 const handler = (event, context, callback) =>
   get_weather()
     .then(response =>
-      Promise.all(['currently', 'minutely', 'hourly', 'daily'].map(k =>
-        new Promise((resolve, reject) => {
-          new AWS.IotData({endpoint: process.env.AWS_IOT_ENDPOINT}).updateThingShadow({
-            thingName: `weather_${k}`,
-            payload: JSON.stringify({
-              state: {
-                desired: response[k]
-              }
-            })
-          }, (err, result) => {
-            if (err)
-              return reject(err)
-            resolve(result)
-          })
-        })
-      ))
+      Promise.all(['currently', 'minutely', 'hourly', 'daily'].map(k => updateIoT(k, response)))
     )
     .catch(err => callback(err))
     .then(responses => callback(null, responses))
+
+const updateIoT = (which, weather) => new Promise((resolve, reject) =>
+  new AWS.IotData({endpoint: process.env.AWS_IOT_ENDPOINT}).updateThingShadow({
+    thingName: `weather_${k}`,
+    payload: JSON.stringify({
+      state: {
+        desired: response[k]
+      }
+    })
+  }, (err, result) => {
+    if (err)
+      return reject(err)
+    resolve(result)
+  })
+)
 
 const get_weather = () => request(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${LAT},${LON}?exclude=flags&units=uk2`)
   .then(JSON.parse)
