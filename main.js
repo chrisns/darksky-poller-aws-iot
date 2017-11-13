973a165f8aa1be5a079cfd257a3dc9892a3e99f8
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-11-13 09:26:04 +0000

    less dependencies

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const AWS = require('aws-sdk')
-const request = require('request-promise')
 const {LAT, LON, DARKSKY_API_KEY} = process.env
 
 const handler = (event, context, callback) =>
@@ -12,10 +11,10 @@ const handler = (event, context, callback) =>
 
 const updateIoT = (which, weather) => new Promise((resolve, reject) =>
   new AWS.IotData({endpoint: process.env.AWS_IOT_ENDPOINT}).updateThingShadow({
-    thingName: `weather_${k}`,
+    thingName: `weather_${which}`,
     payload: JSON.stringify({
       state: {
-        desired: response[k]
+        desired: weather[which]
       }
     })
   }, (err, result) => {
@@ -25,11 +24,29 @@ const updateIoT = (which, weather) => new Promise((resolve, reject) =>
   })
 )
 
-const get_weather = () => request(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${LAT},${LON}?exclude=flags&units=uk2`)
+const get_weather = () => getContent(`https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${LAT},${LON}?exclude=flags&units=uk2`)
   .then(JSON.parse)
   .then(response => {
     response.hourly.data = response.hourly.data.slice(0, 24)
     return response
   })
+
+const getContent = url => new Promise((resolve, reject) => {
+  const lib = url.startsWith('https') ? require('https') : require('http');
+  const request = lib.get(url, (response) => {
+    // handle http errors
+    if (response.statusCode < 200 || response.statusCode > 299) {
+      reject(new Error('Failed to load page, status code: ' + response.statusCode));
+    }
+    // temporary data holder
+    const body = [];
+    // on every content chunk, push it to the data array
+    response.on('data', (chunk) => body.push(chunk));
+    // we are  done, resolve promise with those joined chunks
+    response.on('end', () => resolve(body.join('')));
+  });
+  // handle connection errors of the request
+  request.on('error', (err) => reject(err))
+})
 
 exports.handler = handler
